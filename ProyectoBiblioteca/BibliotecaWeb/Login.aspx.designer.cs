fd2fce071002a709b80ba592f1d4d333fed07a8c
leandro <hideki_hinata-@hotmail.com>
2017-05-30 01:43:33 -0400

    [Add]Css Login
    Css login form

--- ProyectoBiblioteca/BibliotecaWeb/Login.aspx.designer.cs
+++ ProyectoBiblioteca/BibliotecaWeb/Login.aspx.designer.cs
@@ -11,23 +11,5 @@ namespace BibliotecaWeb {
     
     
     public partial class Login {
-        
-        /// <summary>
-        /// Control form1.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
-        
-        /// <summary>
-        /// Control ctrLogin.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Login ctrLogin;
     }
 }
